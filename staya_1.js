8ab430ba80f68354194203876ff8e786f24fbcfb
Евгений Арефьев <111676852+arefyev-evgeniy@users.noreply.github.com>
2022-10-01 17:45:46 +0500

    Add files via upload

---
+++ staya_1.js
@@ -0,0 +1,11 @@
+describe('Сайт staya', function () {
+	it('открытие сайта', function (){
+		cy.visit('https://staya.dog')
+		cy.get('.header-bottom__right--link').click();
+		cy.get('.auth-form > form > [type="email"]').type('fantomcheg777@gmail.com');
+		cy.get('.auth-form > form > [type="password"]').type('Jon777545');
+		cy.get('.auth-form__submit').click();
+		cy.contains('Мои заказы');
+
+	})
+})
