2d03550db185b41b52c4edf8cdb442ba1d77dfba
Евгений Арефьев <111676852+arefyev-evgeniy@users.noreply.github.com>
2022-10-01 17:47:00 +0500

    Update staya_1.js

--- staya_1.js
+++ staya_1.js
@@ -3,7 +3,7 @@ describe('Сайт staya', function () {
 		cy.visit('https://staya.dog')
 		cy.get('.header-bottom__right--link').click();
 		cy.get('.auth-form > form > [type="email"]').type('fantomcheg777@gmail.com');
-		cy.get('.auth-form > form > [type="password"]').type('Jon777545');
+		cy.get('.auth-form > form > [type="password"]').type('Jon******');
 		cy.get('.auth-form__submit').click();
 		cy.contains('Мои заказы');
 
